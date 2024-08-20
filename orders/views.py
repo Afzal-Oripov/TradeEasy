1d0e989d127971dc1eb83c0e0509eb8eabd28743
Afzal <afzaloripov007@gmail.com>
2024-08-20 15:46:22 +0500

    not works email

--- orders/views.py
+++ orders/views.py
@@ -1,27 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import OrderItem
 from .forms import OrderCreateForm
 from cart.cart import Cart
+from .tasks import order_created
+from django.urls import reverse
 
 def order_create(request):
+    # Получаем текущую корзину пользователя
     cart = Cart(request)
     if request.method == 'POST':
-            form = OrderCreateForm(request.POST)
-            if form.is_valid():
-                order = form.save()
-                for item in cart:
-                    OrderItem.objects.create(order=order,
-                        product=item['product'],
-                        price=item['price'],
-                        quantity=item['quantity'])
- # очистить корзину
-                cart.clear()
-                # order_created.delay(order.id)
-                return render(request,
-                    'orders/order/created.html',
-                    {'order': order})
+        # Если запрос POST, создаем форму с данными из запроса
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            # Если форма валидна, сохраняем новый заказ
+            order = form.save()
+            # Для каждого элемента в корзине создаем объект OrderItem
+            for item in cart:
+                OrderItem.objects.create(
+                    order=order,
+                    product=item['product'],
+                    price=item['price'],
+                    quantity=item['quantity']
+                )
+            # Очищаем корзину после создания заказа
+            cart.clear()
+            order_created.delay(order.id)
+            # Отображаем страницу с подтверждением создания заказа
+            request.session['order_id'] = order.id
+            # перенаправить к платежу
+            
     else:
+        # Если запрос не POST, создаем пустую форму для нового заказа
         form = OrderCreateForm()
-        return render(request,'orders/order/create.html',{'cart': cart, 'form': form})
-
-# Create your views here.
+    # Отображаем страницу создания заказа с формой и корзиной
+    return render(request, 'orders/order/created.html', {'cart': cart, 'form': form})
